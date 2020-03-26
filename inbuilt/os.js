e34c7b2f7a44ffdf2318bc3cf99c5b6a7f79d94c
aakashDeveloper <ahanda205@gmail.com>
2020-03-26 01:23:08 +0000

    a

---
+++ inbuilt/os.js
@@ -0,0 +1,12 @@
+var os = require('os');
+console.log(os.platform())
+console.log(os.arch())
+console.log(os.cpus())
+console.log(os.hostname())
+console.log(os.type())
+console.log(os.uptime())
+console.log(os.freemem())
+
+
+
+
---
+++ inbuilt/server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+var server = http.createServer(function(req,res){
+    res.write('<h1>Welcome to Node js  App</h1>');
+    res.end()
+});
+
+server.listen(9800);
