b33d23005dd501e663d6c349c3f2d548895bcc05
aakashDeveloper <ahanda205@gmail.com>
2020-04-10 02:34:36 +0100

    a

--- RestAPi/api.js
+++ RestAPi/api.js
@@ -15,8 +15,10 @@ app.get('/',(req,res) => {
     res.status(200).send('Health Check')
 });
 
-app.get('/users',(req,res) => {
-    db.collection(col_name).find({}).toArray((err,result) => {
+app.get('/users/:id',(req,res) => {
+    var {id}= req.params;
+    var query={id:parseInt(id),isActive:true}
+    db.collection(col_name).find(query).toArray((err,result) => {
         if(err){
             throw err
         }else{
@@ -25,6 +27,35 @@ app.get('/users',(req,res) => {
     })
 })
 
+app.get('/users',(req,res) =>{
+    var query = {};
+    if(req.query.id){
+        query={id:parseInt(req.query.id),isActive:true}
+    }else if(req.query.isActive) {
+        var output = req.query.isActive;
+        var condition;
+        if(output==='false'){
+            condition=false
+        }else if(output==='true'){
+            condition=true
+        }else{
+            condition=true
+        }
+        query={isActive:condition}
+    }
+    else{
+        query={isActive:true}
+    }
+    db.collection(col_name).find(query).toArray((err,result) => {
+        if(err){
+            throw err
+        }else{
+            res.send(result);
+        }
+    })
+})
+
+//Insert
 app.post('/addUser',(req,res) => {
     db.collection(col_name).insert(req.body,(err,result) => {
         if(err){
@@ -35,6 +66,36 @@ app.post('/addUser',(req,res) => {
     })
 })
 
+//update
+app.put('/updateUser',(req,res) => {
+    db.collection(col_name)
+        .findOneAndUpdate({id:parseInt(req.body.id)},{
+            $set:{
+                "id":req.body.id,
+                "name":req.body.name,
+                "class":req.body.id,
+                isActive:true
+            }
+        },(err,result) => {
+            if(err){
+                throw err;
+            }else{
+                res.send('Data Update');
+            }
+        })
+})
+
+//Delete
+app.delete('/deleteUser',(req,res) => {
+    db.collection(col_name).findOneAndDelete({"id":parseInt(req.body.id)},
+        (err,result) => {
+        if(err){
+            throw err;
+        }else{
+            res.send('Data Deleted');
+        }
+    })
+})
 
 
 MongoClient.connect(mongourl,(err,client) => {
