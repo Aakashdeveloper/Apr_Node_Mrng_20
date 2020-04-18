36c2f28efdf26f9941e753b697f2f0190a477c2f
aakashDeveloper <ahanda205@gmail.com>
2020-04-18 02:09:34 +0100

    a

--- loginapp/auth/AuthController.js
+++ loginapp/auth/AuthController.js
@@ -27,6 +27,41 @@ router.post('/register',(req,res)=>{
     })
 })
 
+router.post('/login',(req,res) => {
+    User.findOne({email:req.body.email},(err,user)=>{
+        if(err) return res.status(401).send('Login Error')
+        if(!user){
+            const msg ='User Does Not Exist'
+            res.send({auth:false,msg})
+        }else{
+            const passWordIsValid = bcrypt.compareSync(req.body.password,user.password)
+            if(!passWordIsValid){
+                const msg = encodeURIComponent('Invalid Password')
+                res.send({auth:false,msg})
+            }else{
+                var token = jwt.sign({id:user._id}, config.secert,{
+                    expiresIn:86400//24 hours
+                })
+                res.send({auth:true,msg:token})
+            }
+        }
+    })
+})
+
+router.get('/getUser',(req,res) => {
+    var token = req.headers['x-access-token'];
+    if(!token) res.status(401).send({auth:false,msg:'No Token Provided'});
+
+    jwt.verify(token,config.secert,(err,decode) => {
+        if(err) res.status(401).send({auth:false,msg:'Invalid Token Provided'});
+        User.findById(decode.id,{password:0},(err,user)=>{
+            if(err) res.status(500).send('Problem finding user');
+            if(!user) res.status(500).send({auth:false,msg:'No User found'});
+            res.send(user)
+        })
+    })
+})
+
 router.get('/users',(req,res) => {
     User.find({},(err,user)=>{
         if(err) throw err;
